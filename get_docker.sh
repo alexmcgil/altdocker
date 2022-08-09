89e8c091be619c3fa692a18b0f19499a85682c10
alexmcgil <alexmcgil@vivaldi.net>
2022-08-09 12:45:40 +0300

    fix version of docker

--- get_docker.sh
+++ get_docker.sh
@@ -2,10 +2,10 @@
 set -e
 set -x
 
-DOCKER_VERSION=1.12.1
+DOCKER_VERSION=20.10.9
 
 if [ ! -s docker-${DOCKER_VERSION}.tgz ]; then
-    curl -o docker-${DOCKER_VERSION}.tgz https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}.tgz
+    curl -o docker-${DOCKER_VERSION}.tgz https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz
 fi
 
 tar -xzvf docker-${DOCKER_VERSION}.tgz
